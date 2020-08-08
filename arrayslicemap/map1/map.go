f297478c208e123b050ec303b05d8a588dd9e589
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-08-08 12:37:19 -0300

    finalization of the array, slice and map

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[12345678975] = "Prdro"
+	aprovados[12345678555] = "Ana"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[12345678555])
+	delete(aprovados, 12345678555)
+	fmt.Println(aprovados[12345678555])
+}
---
+++ arrayslicemap/mapaninhado/mapaninhado.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	funcsPorLetra := map[string]map[string]float64{
+		"G": {
+			"Gabriela Silva": 15456.78,
+			"Guga Pereira":   8456.78,
+		},
+		"J": {
+			"João Pedro": 11325.45,
+		},
+		"P": {
+			"Pedro Junior": 1200.0,
+		},
+	}
+
+	delete(funcsPorLetra, "P")
+
+	for letra, funcs := range funcsPorLetra {
+		fmt.Println(letra, funcs)
+	}
+
+	for _, funcs := range funcsPorLetra {
+		for nome, salario := range funcs {
+			fmt.Println(nome, salario)
+		}
+	}
+}
