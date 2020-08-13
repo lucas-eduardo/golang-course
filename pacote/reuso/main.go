576f420d9452ed57fad0237a5b0566697944902b
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-08-13 18:36:14 -0300

    completion of the package lesson

---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lucas-eduardo/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	// fmt.Println(area._TrianguloEq(5.0, 2.0))
+}
